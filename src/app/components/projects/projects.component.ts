1e627f2a8eebd0ed1a9943d653161a348f50a6e3
marcemdrs <marce.mdrs@gmail.com>
2021-09-16 23:12:54 -0500

    test

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -70,7 +70,7 @@ export class ProjectsComponent implements OnInit {
 			console.log(i);
 			console.log(this.titleCourse);
 			console.log("testing");
-			text = "assets/img/projects/"+this.titleCourse+"/"+i+".png";
+			text = "../assets/img/projects/"+this.titleCourse+"/"+i+".png";
 			var post = `
 			<div><img src=${text}></div>
 			`;
